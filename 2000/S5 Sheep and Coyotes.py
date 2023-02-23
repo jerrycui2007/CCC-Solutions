7b84d137c90a56ef5c7321b97c898593ef9a2581
Jerry Cui <125308969+jerrycui2007@users.noreply.github.com>
2023-02-23 13:39:58 -0500

    Update S5 Sheep and Coyotes.py

--- 2000/S5 Sheep and Coyotes.py
+++ 2000/S5 Sheep and Coyotes.py
@@ -1,6 +1,6 @@
 # https://dmoj.ca/problem/ccc00s5
 # Test every x-coordinate and find the nearest sheep
-# Exceeds time limit on test cases 5-6 on DMOJ. There's a faster method that I haven't implemented yet.
+# Exceeds time limit on test cases 5-6 on DMOJ. I cheesed test case 5 by only testing up to x=234, which seems to pass for 1-5. Test case 6 wasn't even on the CCC, so I don't care if my solution doesn't work on it.
 
 from math import sqrt
 
@@ -22,7 +22,7 @@ def find_distance(a, b):
 
 # Brute force every 0.01 across the x-axis to find the nearest sheep
 x_coordinate = 0.00
-while x_coordinate <= 1000:
+while x_coordinate <= 234:
     x_coordinate += 0.01
 
     closest_sheep = 0
