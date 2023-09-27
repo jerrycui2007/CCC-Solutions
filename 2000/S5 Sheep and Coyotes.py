180c03d0583ea0b29c0ca88a4e0186e2b7fd540c
Jerry Cui <125308969+jerrycui2007@users.noreply.github.com>
2023-09-26 21:51:21 -0400

    Updated 2000S5 with a better solution

--- 2000/S5 Sheep and Coyotes.py
+++ 2000/S5 Sheep and Coyotes.py
@@ -1,62 +1,66 @@
 # https://dmoj.ca/problem/ccc00s5
-# Test every x-coordinate and find the nearest sheep
-# Exceeds time limit on test cases 5-6 on DMOJ. I cheesed test case 5 by only testing up to x=234, which seems to pass for 1-5. Test case 6 wasn't even on the CCC, so I don't care if my solution doesn't work on it.
+# Still does not pass test case 6, due to some bs floating-point imprecision. Better than my previous brute force
+# solution, though.
 
-from math import sqrt
+# Basically, for each sheep, calculate the range on the x-axis for which it is the nearest sheep, and mark safe
+# the sheep that are safe
+
+
+class Sheep:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+        self.eaten = True  # assume all sheep are targets first
+
+    def equals(self, other):  # Two sheep are the same if they have the same coordinate
+        if (self.x == other.x) and (self.y == other.y):
+            return True
+        else:
+            return False
+
 
 number_of_sheep = int(input())
-sheep_positions = []
+sheep = []
 for _ in range(number_of_sheep):
-    sheep_positions.append((float(input()), float(input())))  # x and y coordinates
-eaten_sheep = []
+    sheep.append(Sheep(float(input()), float(input())))  # x and y coordinates
 
 
-def find_distance(a, b):
-    # Find the distance between two points a (x, y) and b (x, y)
-    base = b[0] - a[0]
-    height = b[1] - a[1]
-    distance = sqrt(base ** 2 + height ** 2)
+for current_sheep in sheep:
+    # range of x-values in which this sheep is closest to the fence
+    left_range = 0
+    right_range = 1000
 
-    return distance
+    for other_sheep in sheep:
+        if current_sheep.eaten and other_sheep.eaten and (not current_sheep.equals(other_sheep)):
+            x_midpoint = (current_sheep.x + other_sheep.x) / 2
+            y_midpoint = (current_sheep.y + other_sheep.y) / 2
 
+            try:
+                perpendicular_slope = (current_sheep.x - other_sheep.x) / (other_sheep.y - current_sheep.y)
+            except ZeroDivisionError:
+                continue
 
-# Brute force every 0.01 across the x-axis to find the nearest sheep
-x_coordinate = 0.00
-while x_coordinate <= 234:
-    x_coordinate += 0.01
+            if perpendicular_slope == 0:  # the two sheep have the same x-value
+                if current_sheep.y < other_sheep.y:
+                    other_sheep.eaten = False
+                else:
+                    current_sheep.eaten = False
+            else:
+                intersection_point = -y_midpoint / perpendicular_slope + x_midpoint  # intersection with x-axis
 
-    closest_sheep = 0
-    longest_distance = 9e9  # any large number
-    for sheep in sheep_positions:
-        distance = find_distance((x_coordinate, 0), sheep)
-        if distance < longest_distance:
-            closest_sheep = sheep
-            longest_distance = distance
+                if other_sheep.x < current_sheep.x:
+                    left_range = max(left_range, intersection_point)
+                else:
+                    right_range = min(right_range, intersection_point)
 
-    if closest_sheep not in eaten_sheep:
-        eaten_sheep.append(closest_sheep)
+    if left_range >= right_range:  # no possible range for this sheep to be eaten from
+        current_sheep.eaten = False
 
-for sheep in eaten_sheep:
-    # print("The sheep at ({0}, {1}) might be eaten.".format(round(sheep[0], 3), round(sheep[1], 3)))
-    sheep0 = "{:.2f}".format(sheep[0])
-    sheep1 = "{:.2f}".format(sheep[1])
-    print("The sheep at (", sheep0, ", ", sheep1, ") might be eaten.", sep="")
+for current_sheep in sheep:
+    if current_sheep.eaten:
+        # decimal formatting
+        sheep_x = "{:.2f}".format(current_sheep.x)
+        sheep_y = "{:.2f}".format(current_sheep.y)
 
-# Test case:
-"""
-7
-100.00
-100.00
-200.00
-150.00
-100.00
-301.00
-301.00
-301.00
-301.00
-100.00
-141.42
-200.5
-141.43
-200.5
-"""
+        print("The sheep at ({0}, {1}) might be eaten.".format(sheep_x, sheep_y))
